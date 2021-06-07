b61d47dc3e4b427e5aee221e23f1f27372a6e9c5
sakhaouth <65571206+sakhaouth@users.noreply.github.com>
2021-06-07 23:14:09 +0600

    Added homeActivity  and set it launcher and create java code directory

---
+++ app/src/main/java/edu/univdhaka/cse/cse2216/myshop/Home.java
@@ -0,0 +1,14 @@
+package edu.univdhaka.cse.cse2216.myshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
